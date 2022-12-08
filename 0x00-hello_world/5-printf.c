4540ed17158193beec03967968ddd20df98de74f
minasealx <minasealx@gmail.com>
2022-12-08 11:36:59 +0300

    hello. print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/**
+ * main - prints  something
+ * return - program ends here
+ *
+ */
+
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return(0);
+}
+	
+
