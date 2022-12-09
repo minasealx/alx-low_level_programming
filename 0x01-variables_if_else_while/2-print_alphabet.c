56c2dd4c2b089f4abbdc7d4b931911896538f172
minasealx <minasealx@gmail.com>
2022-12-09 14:29:17 +0300

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints alphabet
+ * Return: ends the program
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 97; i < 123; i++)
+{
+putchar (i);
+}
+
+return (0);
+}
