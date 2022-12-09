6414cd3d7587560f00e620bb75fc920bfcfbd466
minasealx <minasealx@gmail.com>
2022-12-09 15:49:42 +0300

    fifth

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- prints numbers
+ * Return: ends the program
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+printf("%d\n", i);
+}
+
+return (0);
+}
