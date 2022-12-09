7209c60c626eaaa6b273ae619cbbb8f1ac1b6dd3
minasealx <minasealx@gmail.com>
2022-12-09 18:20:16 +0300

    adv 2

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,12 +17,12 @@ for (tens = '0'; tens <= '9'; tens++)
 {
 for (ones = '0'; ones <= '9'; ones++)
 {
-if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens))) /*eliminates repitition*/
+if (!((ones == tens) || (tens == hundreds) || (tens > ones) || (hundreds > tens)))
 {
 putchar(hundreds);
 putchar(tens);
 putchar(ones);
-if (!(ones == '9' && hundreds == '7' && tens == '8')) /*adds commas and spaces*/
+if (!(ones == '9' && hundreds == '7' && tens == '8'))
 {
 putchar(',');
 putchar(' ');
