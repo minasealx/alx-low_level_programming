6889680355cea6b120320318fa2d22b46e8a478d
minasealx <minasealx@gmail.com>
2022-12-09 15:57:59 +0300

    sixz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+include < stdio.h >
+/**
+ * main - prints alphabet
+ * Return: ends the program
+ */
+
+int main(void)
+{
+int i;
+
+for (i = 48; i < 58; i++)
+{
+putchar (i);
+}
+putchar (10);
+
+return (0);
+}
