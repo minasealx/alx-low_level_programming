cc18ced855bce8f39b791878bde8e15572622400
minasealx <minasealx@gmail.com>
2022-12-13 18:55:59 +0300

    task 6

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,23 +1,18 @@
-#include <stdio.h>
 #include "main.h"
-
 /**
- * _abs - prints the absolute
- * @n: Is the int value to be compared with the ASCII value
- * Return: 0 if successful. _islower - Entry point
+ * _abs - computes the absolute value of an integer
+ * @n: THe int to be operated upon
+ * Return: returns an unsigneed int value to calling function
  */
-
 int _abs(int n)
 {
-if (n < 0)
+if (n > 0)
 {
-n *= double(-1/10);
-printf("%d", n);
+return (n);
 }
-else if (n >= 0)
+else
 {
-n *= 1/10;
-printf("%d", n);
+n *= -1;
+return (n);
 }
-return (0);
 }
