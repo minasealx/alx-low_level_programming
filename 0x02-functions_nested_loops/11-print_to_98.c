5a0342d0493359fac264405934e18015971cade6
minasealx <minasealx@gmail.com>
2022-12-13 23:24:19 +0300

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - Starts here
+ * @n: initial number
+ * Return: returns 0
+ */
+
+void print_to_98(int n)
+{
+int i;
+for (i = n; i <= 98; i++)
+printf("%d", i);
+return (0);
+}
