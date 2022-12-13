002d62801bcef7badd54de31a34920dff87f86ad
minasealx <minasealx@gmail.com>
2022-12-13 23:13:01 +0300

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two number
+ * Return: 0 it complates
+ * @m: first number
+ * @k: second number
+ */
+
+int add(int m, int k)
+{
+int n = m + k;
+return (n);
+}
