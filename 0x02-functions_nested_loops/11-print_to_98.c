3e034c824971cac8ca6d001a42ba0737bc10f506
minasealx <minasealx@gmail.com>
2022-12-13 23:48:52 +0300

    task 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,16 +12,18 @@ void print_to_98(int n)
 int i;
 if (n <= 98)
 {
-for (i = n; i <= 98; i++)
+for (i = n; i <= 97; i++)
 {
 printf("%d, ", i);
 }
+printf("%d", 98);
 }
 else
 {
-for (i = n; i >= 98; i--)
+for (i = n; i >= 99; i--)
 {
 printf("%d, ", i);
 }
+printf("%d", 98);
 }
 }
