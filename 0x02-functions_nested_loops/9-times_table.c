24b1a8d377911057c1a2487e3746b90311823452
minasealx <minasealx@gmail.com>
2022-12-14 10:53:12 +0300

    last 3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -5,42 +5,43 @@
  */
 void times_table(void)
 {
-int a, b, c;
-for (a = 0; a <= 9; a++)
-{
-for (b = 0; b <= 9; b++)
-{
-c = a * b;
-if ((c / 10) == 0)
-{
-if (b == 0)
-{
-_putchar('0');
-}
+	int a, b, c;
 
-if (b != 0)
-{
-_putchar(' ');
-_putchar((c % 10) + '0');
-}
-if (b < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-else
-{
-_putchar((c / 10) + '0');
-_putchar((c % 10) + '0');
-if (b < 9)
-{
-_putchar(',');
-_putchar(' ');
-}
-}
-}
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			c = a * b;
+			if ((c / 10) == 0)
+			{
+				if (b == 0)
+				{
+					_putchar('0');
+				}
 
-_putchar('\n');
-}
+				if (b != 0)
+				{
+					_putchar(' ');
+					_putchar((c % 10) + '0');
+				}
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((c / 10) + '0');
+				_putchar((c % 10) + '0');
+				if (b < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+
+		_putchar('\n');
+	}
 }
