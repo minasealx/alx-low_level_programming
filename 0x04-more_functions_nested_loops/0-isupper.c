64ef1280938bb896ad10da379e4595a3c608df1f
minasealx <minasealx@gmail.com>
2022-12-15 18:51:27 +0300

    task 1

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _isupper - Is the enter
+ * @c: the integer to be checked
+ * Return: the value
+ */
+
+int _isupper(int c)
+{
+	if _isupper(c)
+{
+	return (1);
+}
+	else
+{
+	return (0);
+}
+
