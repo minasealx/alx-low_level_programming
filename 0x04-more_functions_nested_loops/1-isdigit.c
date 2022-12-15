3eb4961152ce3c17a39c8498fd1f2bd37d013f8c
minasealx <minasealx@gmail.com>
2022-12-15 19:09:51 +0300

    task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isdigit - program starts here
+ * @c: to be checked
+ * Return: the result
+ */
+
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+{
+		return (1);
+}
+	else
+{
+	return (0);
+}
+}
