148b9fd91ada0daa21498489059dbf687d68f912
minasealx <minasealx@gmail.com>
2022-12-15 19:32:11 +0300

    task 4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - starts here
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i < 10)
+{
+	_putchar('i');
+	i++;
+}
+	_putchar('\n');
+}
